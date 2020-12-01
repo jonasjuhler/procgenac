ab31bd756c8235ef8ab677727361c1a8adb95d70
Jonas Juhler <jonas.juhler.n@gmail.com>
2020-12-01 15:55:39 +0100

    The training scripts now use the training_pipeline

--- scripts/train_a2c.py
+++ scripts/train_a2c.py
@@ -1,98 +1,27 @@
-import torch
-from procgenac.utils import make_env, save_rewards
-from procgenac.modelling.utils import train_model, save_model, evaluate_model
-from procgenac.modelling.a2c import A2C
-from procgenac.modelling.encoder import Encoder
-import time
+from procgenac.modelling.utils import training_pipeline
 
-before = time.time()
-# Model name
-model_name = "A2C"
 
-# Hyperparameters
-num_epochs = 3
-batch_size = 1024
-adam_lr = 1e-4
-adam_eps = 1e-3
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(f"Running on {device}")
+class A2CParams:
+    def __init__(self):
+        self.model_type = "A2C"
+        self.env_name = "starpilot"
+        self.num_envs = "32"
+        self.num_levels = "200"
+        self.feature_dim = "128"
+        self.value_coef = "0.5"
+        self.entropy_coef = "0.01"
+        self.eps = "0.2"
+        self.grad_eps = "0.5"
+        self.num_epochs = "3"
+        self.batch_size = "1024"
+        self.adam_lr = "2e-4"  # 2e-4 for A2C
+        self.adam_eps = "1e-4"  # 1e-4 for A2C
+        self.num_steps = "256"
+        self.total_steps = "10000"
+        self.get_test = "1"
 
-# Environment hyperparameters
-total_steps = 2_000_000
-num_envs = 32
-num_steps = 256
-num_levels = 200
-env_name = "starpilot"
 
-# Model hyperparameters
-feature_dim = 128  # Length of output feature vector from Encoder
-grad_eps = 0.5  # Clip value for norm of gradients
-value_coef = 0.5  # coefficient in loss
-entropy_coef = 0.01  # coefficient in loss
+path_to_base = "../"
+param_args = A2CParams()
 
-# Define training and test environment (unseen levels)
-env = make_env(n_envs=num_envs, env_name=env_name, num_levels=num_levels)
-eval_env = make_env(
-    num_envs,
-    env_name=env_name,
-    start_level=num_levels,
-    num_levels=num_levels,
-    normalize_reward=False,
-)
-
-# Define network
-a2c_model = A2C(
-    encoder=Encoder(in_channels=3, feature_dim=feature_dim),
-    feature_dim=feature_dim,
-    num_actions=env.action_space.n,
-    c1=value_coef,
-    c2=entropy_coef,
-    grad_eps=grad_eps,
-    device=device,
-)
-
-# Train model
-a2c_model, (steps, rewards) = train_model(
-    model=a2c_model,
-    env=env,
-    device=device,
-    num_epochs=num_epochs,
-    batch_size=batch_size,
-    adam_lr=adam_lr,
-    adam_eps=adam_eps,
-    num_steps=num_steps,
-    total_steps=total_steps,
-    get_test_error=False,
-    eval_env=eval_env,
-    verbose=True,
-)
-print("Completed training!")
-
-# Store training results
-filename = f"{model_name}_{env_name}.csv"
-save_rewards(steps, rewards, filename)
-
-# Save snapshot of current policy
-filename = f"{model_name}_{env_name}.pt"
-save_model(a2c_model, filename)
-
-# Make env for generating a video
-video_env = make_env(
-    n_envs=1,
-    env_name=env_name,
-    start_level=num_levels,
-    num_levels=num_levels,
-    normalize_reward=False,
-)
-obs = video_env.reset()
-total_reward, _ = evaluate_model(
-    model=a2c_model,
-    eval_env=video_env,
-    obs=obs,
-    num_steps=512,
-    video=True,
-    video_filename=f"{model_name}_{env_name}.mp4",
-)
-print("Video return:", total_reward.mean(0).item())
-
-print(f"Time taken: {(time.time() - before)/60:.1f} minutes")
+training_pipeline(param_args, path_to_base, verbose=True, prod=False)
--- scripts/train_ppo.py
+++ scripts/train_ppo.py
@@ -1,100 +1,27 @@
-import torch
-from procgenac.utils import make_env, save_rewards
-from procgenac.modelling.utils import train_model, save_model, evaluate_model
-from procgenac.modelling.ppo import PPO
-from procgenac.modelling.encoder import Encoder
-import time
+from procgenac.modelling.utils import training_pipeline
 
-before = time.time()
-# Model name
-model_name = "PolicyPPO"
 
-# Hyperparameters
-num_epochs = 3
-batch_size = 1024
-adam_lr = 5e-4
-adam_eps = 1e-5
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(f"Running on {device}")
+class PPOParams:
+    def __init__(self):
+        self.model_type = "PPO"
+        self.env_name = "starpilot"
+        self.num_envs = "32"
+        self.num_levels = "200"
+        self.feature_dim = "128"
+        self.value_coef = "0.5"
+        self.entropy_coef = "0.01"
+        self.eps = "0.2"
+        self.grad_eps = "0.5"
+        self.num_epochs = "3"
+        self.batch_size = "1024"
+        self.adam_lr = "5e-4"  # 2e-4 for A2C
+        self.adam_eps = "1e-5"  # 1e-4 for A2C
+        self.num_steps = "256"
+        self.total_steps = "10000"
+        self.get_test = "1"
 
-# Environment hyperparameters
-total_steps = 2_000_000
-num_envs = 32
-num_steps = 256
-num_levels = 200
-env_name = "starpilot"
 
-# Model hyperparameters
-feature_dim = 128  # Length of output feature vector from Encoder
-grad_eps = 0.5  # Clip value for norm of gradients
-eps = 0.2  # needed for clip values in loss function
-value_coef = 0.5  # coefficient in loss
-entropy_coef = 0.01  # coefficient in loss
+path_to_base = "../"
+param_args = PPOParams()
 
-# Define training and test environment (unseen levels)
-env = make_env(n_envs=num_envs, env_name=env_name, num_levels=num_levels)
-eval_env = make_env(
-    n_envs=num_envs,
-    env_name=env_name,
-    start_level=num_levels,
-    num_levels=num_levels,
-    normalize_reward=False,
-)
-
-# Define network
-policy_ppo = PPO(
-    encoder=Encoder(in_channels=3, feature_dim=feature_dim),
-    feature_dim=feature_dim,
-    num_actions=env.action_space.n,
-    c1=value_coef,
-    c2=entropy_coef,
-    eps=eps,
-    grad_eps=grad_eps,
-    device=device,
-)
-
-# Train model
-policy_ppo, (steps, rewards) = train_model(
-    model=policy_ppo,
-    env=env,
-    device=device,
-    num_epochs=num_epochs,
-    batch_size=batch_size,
-    adam_lr=adam_lr,
-    adam_eps=adam_eps,
-    num_steps=num_steps,
-    total_steps=total_steps,
-    get_test_error=False,
-    eval_env=eval_env,
-    verbose=True,
-)
-print("Completed training!")
-
-# Store training results
-filename = f"{model_name}_{env_name}.csv"
-save_rewards(steps, rewards, filename)
-
-# Save snapshot of current policy
-filename = f"{model_name}_{env_name}.pt"
-save_model(policy_ppo, filename)
-
-# Make env for video
-video_env = make_env(
-    n_envs=1,
-    env_name=env_name,
-    start_level=num_levels,
-    num_levels=num_levels,
-    normalize_reward=False,
-)
-obs = video_env.reset()
-total_reward, _ = evaluate_model(
-    model=policy_ppo,
-    eval_env=video_env,
-    obs=obs,
-    num_steps=512,
-    video=True,
-    video_filename=f"{model_name}_{env_name}.mp4",
-)
-print("Average return:", total_reward.mean(0).item())
-
-print(f"Time taken: {(time.time() - before)/60:.1f} minutes")
+training_pipeline(param_args, path_to_base, verbose=True, prod=False)
