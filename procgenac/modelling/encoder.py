a47fb502a83cf24f0f47221669b5db9516b06abf
Jonas Juhler <jonas.juhler.n@gmail.com>
2020-12-06 19:29:04 +0100

    Added Impala CNN as encoder

--- procgenac/modelling/encoder.py
+++ procgenac/modelling/encoder.py
@@ -7,7 +7,7 @@ class Flatten(nn.Module):
         return x.view(x.size(0), -1)
 
 
-class Encoder(nn.Module):
+class Nature(nn.Module):
     def __init__(self, in_channels, feature_dim):
         super().__init__()
         self.layers = nn.Sequential(
@@ -25,3 +25,88 @@ class Encoder(nn.Module):
 
     def forward(self, x):
         return self.layers(x)
+
+
+class Impala(nn.Module):
+    def __init__(self, in_channels, feature_dim):
+        super().__init__()
+
+        self.sequence1 = nn.Sequential(
+            nn.Conv2d(in_channels=in_channels, out_channels=16, kernel_size=3, stride=1, padding=1),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+        )
+
+        self.res_block16_1 = nn.Sequential(
+            nn.ReLU(),
+            nn.Conv2d(in_channels=16, out_channels=16, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=16, out_channels=16, kernel_size=3, stride=1, padding=1),
+        )
+
+        self.res_block16_2 = nn.Sequential(
+            nn.ReLU(),
+            nn.Conv2d(in_channels=16, out_channels=16, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=16, out_channels=16, kernel_size=3, stride=1, padding=1),
+        )
+
+        self.sequence2 = nn.Sequential(
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+        )
+
+        self.res_block32_11 = nn.Sequential(
+            nn.ReLU(),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=1, padding=1),
+        )
+
+        self.res_block32_12 = nn.Sequential(
+            nn.ReLU(),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=1, padding=1),
+        )
+
+        self.sequence3 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=1, padding=1),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+        )
+
+        self.res_block32_21 = nn.Sequential(
+            nn.ReLU(),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=1, padding=1),
+        )
+
+        self.res_block32_22 = nn.Sequential(
+            nn.ReLU(),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=1, padding=1),
+        )
+        self.finalize = nn.Sequential(
+            Flatten(),
+            nn.Linear(in_features=2048, out_features=feature_dim),
+            nn.ReLU(),
+        )
+        self.apply(orthogonal_init)
+
+    def forward(self, x):
+        x = self.sequence1(x)
+        x = x + self.res_block16_1(x)
+        x = x + self.res_block16_2(x)
+
+        x = self.sequence2(x)
+        x = x + self.res_block32_11(x)
+        x = x + self.res_block32_12(x)
+
+        x = self.sequence3(x)
+        x = x + self.res_block32_21(x)
+        x = x + self.res_block32_22(x)
+
+        x = self.finalize(x)
+
+        return x
