4790b87cae089d962fd9925a91215458928d1639
Jonas Juhler-Nottrup <jonas.juhler.n@gmail.com>
2021-01-02 21:54:47 +0100

    Improved script a little bit

--- scripts/plot_model_reward.py
+++ scripts/plot_model_reward.py
@@ -15,24 +15,24 @@ plt.rc("ytick", labelsize=SMALL_SIZE)  # fontsize of the tick labels
 plt.rc("legend", fontsize=SMALL_SIZE)  # legend fontsize
 plt.rc("figure", titlesize=BIGGER_SIZE)  # fontsize of the figure title
 
-idxs = [6, 3]
-k = "cnn_type"
-v = ["impala", "nature"]
+idxs = [6, 8]
+k = "model_type"
+v = ["A2C", "PPO"]
 
-mt = "A2C"
+mts = ["A2C", "PPO"]
 fig, ax = plt.subplots(1, 1, figsize=(7, 4))
-cols = [["darkslategrey", "lightskyblue"], ["maroon", "darksalmon"]]
+cols = [["darkslategrey", "lightskyblue"], ["darksalmon", "maroon"]]
 
 for i, hp in enumerate(v):
     idx = idxs[i]
-    rew_df = pd.read_csv(f"../results/rewards/{mt}_id{idx}_starpilot.csv")
+    rew_df = pd.read_csv(f"../results/rewards/{mts[i]}_id{idx}_starpilot.csv")
     k_lab = k.replace("_", "\\_")
     ax = plot_results(
         ax,
         rew_df,
         colors=cols[i],
         include_train=False,
-        include_std=True,
+        include_std=False,
         model_label=f"{k_lab}={hp}",
     )
 
@@ -40,7 +40,7 @@ formatter = get_formatter()
 ax.xaxis.set_major_formatter(formatter)
 ax.set_xlabel("Steps")
 ax.set_ylabel("Reward")
+plt.tight_layout()
 plt.legend()
-figpath = "../results/figures/A2C_impala_nature.png"
-plt.savefig(figpath, dpi=600)
-plt.show()
+figpath = "../results/figures/a2c_v_ppo.pdf"
+plt.savefig(figpath)
