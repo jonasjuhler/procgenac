8aece559f0a6e213348867688e6c9860eae13ed1
Jonas Juhler-Nottrup <jonas.juhler.n@gmail.com>
2021-01-04 15:29:56 +0100

    changed back to original state

--- scripts/plot_model_reward.py
+++ scripts/plot_model_reward.py
@@ -15,17 +15,17 @@ plt.rc("ytick", labelsize=SMALL_SIZE)  # fontsize of the tick labels
 plt.rc("legend", fontsize=SMALL_SIZE)  # legend fontsize
 plt.rc("figure", titlesize=BIGGER_SIZE)  # fontsize of the figure title
 
-idxs = [6, 8]
-k = "model_type"
-v = ["A2C", "PPO"]
+idxs = [12, 15]
+k = "grad_eps"
+v = ["0.7", "0.5"]
 
-mts = ["A2C", "PPO"]
+mt = "PPO"
 fig, ax = plt.subplots(1, 1, figsize=(7, 4))
 cols = [["darkslategrey", "lightskyblue"], ["darksalmon", "maroon"]]
 
 for i, hp in enumerate(v):
     idx = idxs[i]
-    rew_df = pd.read_csv(f"../results/rewards/{mts[i]}_id{idx}_starpilot.csv")
+    rew_df = pd.read_csv(f"../results/rewards/{mt}_id{idx}_starpilot.csv")
     k_lab = k.replace("_", "\\_")
     ax = plot_results(
         ax,
@@ -42,5 +42,5 @@ ax.set_xlabel("Steps")
 ax.set_ylabel("Reward")
 plt.tight_layout()
 plt.legend()
-figpath = "../results/figures/a2c_v_ppo.pdf"
+figpath = f"../results/figures/{mt}_{k}_compare.pdf"
 plt.savefig(figpath)
